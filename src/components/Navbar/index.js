3e7133cc13d49d95e94f986b5625d3cc459ff5f8
sumedhashambhavi <shambhavisumedha@gmail.com>
2022-05-25 16:42:42 +0530

    initial build

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/about-us' activestyle="true">
+			About
+		</NavLink>
+		<NavLink to='/contact-us' activestyle="true">
+			Contact
+		</NavLink>
+		<NavLink to='/home' activestyle="true">
+			Home
+		</NavLink>
+		{/* Second Nav */}
+		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+		</NavMenu>
+		<NavBtn>
+		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
+		</NavBtn>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
---
+++ src/components/workspace.js
@@ -0,0 +1,65 @@
+import { Component } from "react";
+import Image from "react";
+
+
+class Workspace extends Component {
+    
+    state = {
+        selectedFile: null
+    };
+    
+    onFileChange = event => {
+        // this.setState({ selectedFile: event.target.files[0] });
+        this.setState({selectedFile: URL.createObjectURL(event.target.files[0])})
+    };
+    
+    onFileUpload = () => {
+        // const formData = new FormData();
+        // formData.append(
+        //     "myFile",
+        //     this.state.selectedFile,
+        //     this.state.selectedFile.name
+        //     );
+        console.log(this.state.selectedFile);
+    };
+    
+    fileData = () => {
+        if (this.state.selectedFile) {
+            return (
+                <div>
+                    <img src={this.state.selectedFile} />
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <br />
+                    <h4>Choose before Pressing the Upload button</h4>
+                </div>
+            );
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>
+                GeeksforGeeks
+                </h1>
+                <h3>
+                File Upload using React!
+                </h3>
+                <div>
+                    <input type="file" onChange={this.onFileChange} />
+                    <button onClick={this.onFileUpload}>
+                    Upload!
+                    </button>
+                </div>
+            {this.fileData()}
+            </div>
+        );
+    }
+}
+
+  
+  export default Workspace;
